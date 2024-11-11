0b788ab6aa1bd289416767f929f91cec7b28ac6c
joelfilho <joelfilho@ciandt.com>
2024-11-11 20:31:01 -0300

    feat: add toast

---
+++ apps/web/src/shared/utils/notifications.ts
@@ -0,0 +1,26 @@
+import { AxiosError } from "axios";
+import { toast } from "sonner";
+
+export const notifySuccess = () => {
+  toast.success("Sucesso!", {
+    description: "Operação realizada com sucesso.",
+    duration: 1500,
+  });
+};
+
+export const notifyError = (e: AxiosError) => {
+  const { response } = e;
+
+  if (response?.status === 500) {
+    return toast.error("Oops! Algo deu errado.", {
+      description:
+        "Verifique sua conexão com a internet e tente novamente mais tarde.",
+      duration: 1500,
+    });
+  }
+
+  toast.warning("Atenção!", {
+    description: e.message,
+    duration: 1500,
+  });
+};
