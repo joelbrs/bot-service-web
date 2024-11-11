06b8fb160fb1a2812769647dd1908672d37c0408
joelfilho <joelfilho@ciandt.com>
2024-11-11 19:09:18 -0300

    feat: add separator between page title and content

--- apps/web/src/shared/layouts/page.tsx
+++ apps/web/src/shared/layouts/page.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components";
+import { Separator } from "@repo/ui/components";
 
 type Props = {
   title: string;
@@ -12,7 +13,8 @@ export const Page = ({ title, description }: Props): JSX.Element => {
       <Header />
       <div className="sm:px-10 px-5">
         <h1 className="text-3xl font-bold mt-5">{title}</h1>
-        <p className="text-sm text-muted-foreground mb-5">{description}</p>
+        <p className="text-sm text-muted-foreground">{description}</p>
+        <Separator className="mb-5 mt-2" />
 
         <Outlet />
       </div>
